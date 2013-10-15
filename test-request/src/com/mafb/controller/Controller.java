de64cf11375214cfa1405e51eb7f6cfd3f035c92
irsm <ibraim@linksaude.com>
2013-10-15 15:22:33 -0300

    Segundo commit!

--- test-request/src/com/mafb/controller/Controller.java
+++ test-request/src/com/mafb/controller/Controller.java
@@ -3,7 +3,7 @@ package com.mafb.controller;
 public class Controller {
 
 	public static void main(String[] args) {
-		System.out.println("De novo cara. Certinho.");
+		System.out.println("De novo cara. Certinho!");
 
 	}
 
